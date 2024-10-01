86e76397175a35af251f9264490c097fb38b4c9f
Lucas Vieira <vieira.lucasmm@gmail.com>
2024-09-30 22:01:59 -0300

    Added User domain, and post method

--- src/main/java/com/sprint_tutorial/first_spring_app/controller/HelloWorldController.java
+++ src/main/java/com/sprint_tutorial/first_spring_app/controller/HelloWorldController.java
@@ -1,15 +1,27 @@
 package com.sprint_tutorial.first_spring_app.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.sprint_tutorial.first_spring_app.domain.User;
+import com.sprint_tutorial.first_spring_app.service.HelloWorldService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/hello-world")
 public class HelloWorldController {
 
+    @Autowired
+    private HelloWorldService helloWorldService;
+
     @GetMapping
     public String helloWorld() {
-        return "Hello World";
+        return this.helloWorldService.helloWorld("Lucas");
+    }
+
+    @PostMapping("/{id}")
+    public String helloWorldPost(@PathVariable("id") String id,
+                                 @RequestParam(value = "filter", defaultValue = "name") String filter,
+                                 @RequestBody User body) {
+        return "Hello World Post " + body.getName() + " id: " + id + " filter: " + filter;
     }
 }
